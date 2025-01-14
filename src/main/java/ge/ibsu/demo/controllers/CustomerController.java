dd07503e885f8adfa523f519fdfe044bbecf35ec
IBSU Lecturer <IBSU_Lecturer@ibsu.edu.ge>
2025-01-14 09:12:16 +0400

    add-validations

--- src/main/java/ge/ibsu/demo/controllers/CustomerController.java
+++ src/main/java/ge/ibsu/demo/controllers/CustomerController.java
@@ -1,10 +1,15 @@
 package ge.ibsu.demo.controllers;
 
 import ge.ibsu.demo.dto.AddCustomer;
+import ge.ibsu.demo.dto.RequestData;
+import ge.ibsu.demo.dto.SearchCustomer;
 import ge.ibsu.demo.entities.Customer;
 import ge.ibsu.demo.services.CustomerService;
+import ge.ibsu.demo.util.GeneralUtil;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -28,12 +33,19 @@ public class CustomerController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = {"application/json"})
-    public Customer add(@RequestBody AddCustomer addCustomer) {
+    public Customer add(@RequestBody AddCustomer addCustomer) throws Exception {
+        GeneralUtil.checkRequiredProperties(addCustomer, Arrays.asList("firstName", "lastName"));
         return customerService.addEditCustomer(addCustomer, null);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = {"application/json"})
-    public Customer edit(@RequestBody AddCustomer addCustomer, @PathVariable Long id) {
+    public Customer edit(@RequestBody AddCustomer addCustomer, @PathVariable Long id) throws Exception {
+        GeneralUtil.checkRequiredProperties(addCustomer, Arrays.asList("firstName", "lastName"));
         return customerService.addEditCustomer(addCustomer, id);
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.POST, produces = {"application/json"})
+    public Page<Customer> search(@RequestBody RequestData<SearchCustomer> rd) {
+        return customerService.search(rd.getData(), rd.getPaging());
     }
 }
